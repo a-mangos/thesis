01cbd4143a241e4e933e863f9b8e564ca3a54b0d
a-mangos <alexandermangos212@gmail.com>
2024-03-01 16:05:10 +1100

    trying to make new gui work but completely broken. Also rotation still cooked

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import open3d as o3d
+import open3d.visualization.gui as gui
+import open3d.visualization.rendering as rendering
 import numpy as np
 import serial
 import math
@@ -7,8 +9,7 @@ from struct import unpack
 from statistics import mean
 from scipy.stats import circmean
 from dataclasses import dataclass
-from ahrs.filters import AQUA
-from ahrs import Quaternion
+import vis_gui
 
 
 @dataclass
@@ -20,7 +21,6 @@ class AccelerationData:
     x_cal: int
     y_cal: int
     z_cal: int
-
 
 def open_serial(ser):
     ser.baudrate = 115200
@@ -95,34 +95,48 @@ def calibrate_and_assign_acceleration_data(data_x, data_y, data_z):
 
 if __name__ == "__main__":
     # Initialise serial
-    ser = serial.Serial()
-    open_serial(ser)
+    # ser = serial.Serial()
+    # open_serial(ser)
 
+    gui.Application.instance.initialize()
+    app = vis_gui.AppWindow(1024, 768)
+    # window = gui.Application.instance.create_window("Birth Simulator", width=1000, height=1000)
+    # widget = gui.SceneWidget()
+    # widget.scene = rendering.Open3DScene(window.renderer)
+    # window.add_child(widget)
+    path = "whole_head_model.STL"
+
+    app.load(path)
     # Read in file
-    fetal_head = o3d.io.read_triangle_mesh("whole_head_model.STL")
+    # fetal_head = o3d.io.read_triangle_mesh(path)
+    #
+    # # Initial offset from original model to line up the head with axes of render
+    # # offsets: flexion -0.4 rotation -0.615 deflection -0.2
+    # initial_position = fetal_head.get_rotation_matrix_from_xyz((-0.4, -0.615, -0.2))
+    # fetal_head.rotate(initial_position, fetal_head.get_center())
+    #
+    # # Copy head for dynamic rotation
+    # fetal_head_r = copy.deepcopy(fetal_head)
+    # fetal_head_r.compute_vertex_normals()
+    # fetal_head_r.paint_uniform_color([0.7, 0.7, 0.7])
+    #
+    # material = rendering.MaterialRecord()
+    # material.shader = 'defaultLit'
+    #
+    # # widget.scene.camera.look_at(fetal_head.get_center().tolist(), [1,1,1], [0,0,0])
+    #
+    #
+    # # vis = o3d.visualization.Visualizer()
+    # # vis.create_window(window_name="STL", left=1000, top=200, width=800, height=800)
+    #
+    # coord_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=100, origin=fetal_head_r.get_center())
+    # # vis.add_geometry(coord_frame)
+    # # widget.scene.add_geometry(coord_frame)
+    # widget.scene.add_geometry("head", fetal_head_r, material)
+    # # vis.add_geometry(fetal_head_r)
 
-    # Initial offset from original model to line up the head with axes of render
-    # offsets: flexion -0.4 rotation -0.615 deflection -0.2
-    initial_position = fetal_head.get_rotation_matrix_from_xyz((-0.4, -0.615, -0.2))
-    fetal_head.rotate(initial_position, fetal_head.get_center())
 
-    # Copy head for dynamic rotation
-    fetal_head_r = copy.deepcopy(fetal_head)
-    fetal_head_r.compute_vertex_normals()
-    fetal_head_r.paint_uniform_color([0.7, 0.7, 0.7])
-
-    gui = o3d.visualization.gui.Application.instance
-    gui.initialize()
-
-    vis = o3d.visualization.Visualizer()
-    vis.create_window(window_name="STL", left=1000, top=200, width=800, height=800)
-
-    gui.add_window(vis)
-
-    # MERGE_CLOSE_VERTICES - INTERESTING FOR MERGING COLORS
-    coord_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=100, origin=fetal_head_r.get_center())
-    vis.add_geometry(coord_frame)
-    vis.add_geometry(fetal_head_r)
+    gui.Application.instance.run()
 
     # vis.run()
     # vis.destroy_window()
@@ -132,95 +146,95 @@ if __name__ == "__main__":
     #     for item in vertices_list:
     #         fp.write("%s\n" % item.index)
 
-    points = []
-    with open(r"pin_locations.txt", "r") as f:
-        for line in f:
-            points.append([int(e) for e in line.split()])
-
-    vertices = np.asarray(fetal_head_r.vertices)
-    spheres = []
-    for point in points:
-        point_coord = vertices[point]
-        print(point_coord[0][0], point_coord[0][1], point_coord[0][2])
-        sphere = o3d.geometry.TriangleMesh.create_sphere(radius=6, resolution=5)
-        sphere.translate(np.transpose(point_coord))
-        spheres.append(sphere)
-        vis.add_geometry(sphere)
-
-    # code to paint the head instead of making spheres
-    # vertex_points_to_colour = []
+    # points = []
+    # with open(r"pin_locations.txt", "r") as f:
+    #     for line in f:
+    #         points.append([int(e) for e in line.split()])
+    #
+    # vertices = np.asarray(fetal_head_r.vertices)
+    # spheres = []
     # for point in points:
     #     point_coord = vertices[point]
-    #     vertex_points_to_colour.append(point)
-    #     for index in range(len(vertices) - 1):
-    #         if np.sqrt((vertices[index][0] - point_coord[0][0])**2 +
-    #                    (vertices[index][1] - point_coord[0][1])**2 +
-    #                    (vertices[index][2] - point_coord[0][2])**2) <= 6:
-    #             vertex_points_to_colour.append(index)
-    # vertex_colors = np.asarray(fetal_head_r.vertex_colors)
-
-    view_control = vis.get_view_control()
-    parameters = o3d.io.read_pinhole_camera_parameters("ScreenCamera_2024-02-22-08-39-51.json")
-    view_control.convert_from_pinhole_camera_parameters(parameters, True)
-
-    previous_position = np.identity(3)
-
-    roll_rolling = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    pitch_rolling = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    data_rolling = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-    while True:
-        data = read_from_serial(ser)
-        acceleration_data = calibrate_and_assign_acceleration_data(data[13], data[14], data[15])
-        roll, pitch = calculate_head_angles(acceleration_data)
-
-        roll_rolling.append(roll)
-        roll_rolling.pop(0)
-
-        pitch_rolling.append(pitch)
-        pitch_rolling.pop(0)
-
-        # accel_aqua = AQUA(None, np.asarray((acceleration_data.x_cal, acceleration_data.y_cal, acceleration_data.z_cal)), None)
-        # quaternion = accel_aqua.estimate(accel_aqua.acc, None)
-
-        # print(acceleration_data)
-
-        fetal_head_r.rotate(np.linalg.inv(previous_position), fetal_head_r.get_center())
-        for sphere in spheres:
-            sphere.rotate(np.linalg.inv(previous_position), fetal_head_r.get_center())
-
-        # new_position = fetal_head_r.get_rotation_matrix_from_xyz((0,
-        #                                                           circmean(roll_rolling, high=360) * np.pi / 180,
-        #                                                           0))
-
-        # new_position = fetal_head_r.get_rotation_matrix_from_xyz((mean(pitch_rolling),
-        #                                                           mean(roll_rolling),
-        #                                                           0))
-        new_position = fetal_head_r.get_rotation_matrix_from_xyz((0,
-                                                                  mean(roll_rolling),
-                                                                  0))
-        # new_position = fetal_head_r.get_rotation_matrix_from_quaternion(quaternion)
-
-        for sphere in spheres:
-            sphere.rotate(new_position, fetal_head_r.get_center())
-        fetal_head_r.rotate(new_position, fetal_head_r.get_center())
-        previous_position = new_position
-
-        # data_rolling.append(normalize_data(data[41] * 14 / 512))
-        # data_rolling.pop(0)
-        # for sphere in spheres:
-        #     sphere.paint_uniform_color([mean(data_rolling), 1 - mean(data_rolling), 0])
-
-        for sphere in spheres:
-            sphere.paint_uniform_color([1, 0, 0])
-
-        # for vertex in vertex_points_to_colour:
-        #     vertex_colors[vertex] = [1, 0, 0]
-
-        # fetal_head_r.vertex_colors = o3d.utility.Vector3dVector(vertex_colors)
-
-        vis.update_geometry(fetal_head_r)
-        for sphere in spheres:
-            vis.update_geometry(sphere)
-        vis.update_renderer()
-        vis.poll_events()
+    #     print(point_coord[0][0], point_coord[0][1], point_coord[0][2])
+    #     sphere = o3d.geometry.TriangleMesh.create_sphere(radius=6, resolution=5)
+    #     sphere.translate(np.transpose(point_coord))
+    #     spheres.append(sphere)
+    #     vis.add_geometry(sphere)
+    #
+    # # code to paint the head instead of making spheres
+    # # vertex_points_to_colour = []
+    # # for point in points:
+    # #     point_coord = vertices[point]
+    # #     vertex_points_to_colour.append(point)
+    # #     for index in range(len(vertices) - 1):
+    # #         if np.sqrt((vertices[index][0] - point_coord[0][0])**2 +
+    # #                    (vertices[index][1] - point_coord[0][1])**2 +
+    # #                    (vertices[index][2] - point_coord[0][2])**2) <= 6:
+    # #             vertex_points_to_colour.append(index)
+    # # vertex_colors = np.asarray(fetal_head_r.vertex_colors)
+    #
+    # view_control = vis.get_view_control()
+    # parameters = o3d.io.read_pinhole_camera_parameters("ScreenCamera_2024-02-22-08-39-51.json")
+    # view_control.convert_from_pinhole_camera_parameters(parameters, True)
+    #
+    # previous_position = np.identity(3)
+    #
+    # roll_rolling = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    # pitch_rolling = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    # data_rolling = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    #
+    # while True:
+    #     data = read_from_serial(ser)
+    #     acceleration_data = calibrate_and_assign_acceleration_data(data[13], data[14], data[15])
+    #     roll, pitch = calculate_head_angles(acceleration_data)
+    #
+    #     roll_rolling.append(roll)
+    #     roll_rolling.pop(0)
+    #
+    #     pitch_rolling.append(pitch)
+    #     pitch_rolling.pop(0)
+    #
+    #     # accel_aqua = AQUA(None, np.asarray((acceleration_data.x_cal, acceleration_data.y_cal, acceleration_data.z_cal)), None)
+    #     # quaternion = accel_aqua.estimate(accel_aqua.acc, None)
+    #
+    #     # print(acceleration_data)
+    #
+    #     fetal_head_r.rotate(np.linalg.inv(previous_position), fetal_head_r.get_center())
+    #     for sphere in spheres:
+    #         sphere.rotate(np.linalg.inv(previous_position), fetal_head_r.get_center())
+    #
+    #     # new_position = fetal_head_r.get_rotation_matrix_from_xyz((0,
+    #     #                                                           circmean(roll_rolling, high=360) * np.pi / 180,
+    #     #                                                           0))
+    #
+    #     # new_position = fetal_head_r.get_rotation_matrix_from_xyz((mean(pitch_rolling),
+    #     #                                                           mean(roll_rolling),
+    #     #                                                           0))
+    #     new_position = fetal_head_r.get_rotation_matrix_from_xyz((0,
+    #                                                               mean(roll_rolling),
+    #                                                               0))
+    #     # new_position = fetal_head_r.get_rotation_matrix_from_quaternion(quaternion)
+    #
+    #     for sphere in spheres:
+    #         sphere.rotate(new_position, fetal_head_r.get_center())
+    #     fetal_head_r.rotate(new_position, fetal_head_r.get_center())
+    #     previous_position = new_position
+    #
+    #     # data_rolling.append(normalize_data(data[41] * 14 / 512))
+    #     # data_rolling.pop(0)
+    #     # for sphere in spheres:
+    #     #     sphere.paint_uniform_color([mean(data_rolling), 1 - mean(data_rolling), 0])
+    #
+    #     for sphere in spheres:
+    #         sphere.paint_uniform_color([1, 0, 0])
+    #
+    #     # for vertex in vertex_points_to_colour:
+    #     #     vertex_colors[vertex] = [1, 0, 0]
+    #
+    #     # fetal_head_r.vertex_colors = o3d.utility.Vector3dVector(vertex_colors)
+    #
+    #     vis.update_geometry(fetal_head_r)
+    #     for sphere in spheres:
+    #         vis.update_geometry(sphere)
+    #     vis.update_renderer()
+    #     vis.poll_events()
