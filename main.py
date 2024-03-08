new working gui, have 2 implementations for rotation 1 of which doesn't work because with that I can't update colour too. Rotation not currently implemented and havent yet fixed the gimbal lock problem. New class design for app window

main.py
@@ -9,8 +9,129 @@ from struct import unpack
 from statistics import mean
 from scipy.stats import circmean
 from dataclasses import dataclass
-import vis_gui
 
+
+class AppWindow:
+    def __init__(self):
+        self._app = o3d.visualization.gui.Application.instance
+        self._app.initialize()
+
+        self._window = self._app.create_window("Birth Simulator", width=1280, height=720)
+
+        self._widgetLeft = gui.SceneWidget()
+        self._widgetLeft.scene = rendering.Open3DScene(self._window.renderer)
+        self._window.add_child(self._widgetLeft)
+
+        self._widgetRight = gui.SceneWidget()
+        self._widgetRight.scene = rendering.Open3DScene(self._window.renderer)
+        self._window.add_child(self._widgetRight)
+
+        self._coord_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=100)
+        self._fetal_head = self.load_fetal_head("whole_head_model.stl")
+
+        self._head_pin_locations = read_head_pin_locations_from_file()
+        # dictionary that maps the incoming data input to a pin location.
+        vertices = np.asarray(self._fetal_head.vertices)
+        # map data to vertex at a given index. The index is in pin location
+        # manual assignment prevents the use of two hash maps; firstly mapping pin locations to head sensor spots
+        # then mapping head sensor spots to location on data array
+        # either way would have to manually map the pin locations to head sensor spots
+        # this problem arose because the pin locations given on the VisualiserWithVertexSelection picked_points()
+        # function were random (I don't think I chose them randomly, if I did then manual mapping could be avoided?)
+        # eg. data[0] = B0 = vertices[pin_location[27]]
+        # data[1] =
+
+        # what do i want to do
+        # I have to go through the mesh and find the spheres as I've done before
+        # but when I do it this time, assign each vertex value to the corresponding data point
+        # because when I get the saved vertices, I won't know which one is which
+        # but how do i save it each time
+
+        self._mat = rendering.MaterialRecord()
+        self._mat.shader = 'defaultLit'
+
+        self.set_window_layout()
+        self.setup_scenes()
+
+    def load_fetal_head(self, path):
+        fetal_head = o3d.io.read_triangle_mesh(path)
+        fetal_head.paint_uniform_color([0.7, 0.7, 0.7])
+        fetal_head.compute_vertex_normals()
+
+        return fetal_head
+
+    def setup_scenes(self):
+        self._widgetLeft.scene.clear_geometry()
+        self._widgetRight.scene.clear_geometry()
+
+        self.initial_transform()
+
+        self._widgetLeft.scene.add_geometry("__fetal_head__", self._fetal_head, self._mat)
+        self._widgetLeft.scene.add_geometry("__coord_frame__", self._coord_frame, self._mat)
+        bounds = self._widgetLeft.scene.bounding_box
+        self._widgetLeft.setup_camera(60, bounds, bounds.get_center())
+
+        self._widgetRight.scene.add_geometry("__fetal_head__", self._fetal_head, self._mat)
+        self._widgetRight.scene.add_geometry("__coord_frame__", self._coord_frame, self._mat)
+        bounds = self._widgetRight.scene.bounding_box
+        self._widgetRight.setup_camera(60, bounds, bounds.get_center())
+
+    def set_window_layout(self):
+        r = self._window.content_rect
+        self._widgetLeft.frame = gui.Rect(r.x, r.y, r.width / 2, r.height)
+        self._widgetRight.frame = gui.Rect(r.x + r.width / 2 + 1, r.y, r.width / 2, r.height)
+
+    def initial_transform(self):
+        # Initial offset from original model to line up the head with axes of render
+        # offsets: flexion -0.4 rotation -0.615 deflection -0.2
+        initial_position = self._fetal_head.get_rotation_matrix_from_xyz((-0.4, -0.615, -0.2))
+        self._fetal_head.rotate(initial_position, self._fetal_head.get_center())
+
+        self._coord_frame.translate((0, 0, 0), relative=False)
+        self._fetal_head.translate(self._coord_frame.get_center(), relative=False)
+
+    def update_geometry(self):
+        self._widgetLeft.scene.clear_geometry()
+        self._widgetRight.scene.clear_geometry()
+
+        self.update_pose()
+        self.update_colour()
+
+        self._widgetLeft.scene.add_geometry("__fetal_head__", self._fetal_head, self._mat)
+        self._widgetLeft.scene.add_geometry("__coord_frame__", self._coord_frame, self._mat)
+
+        self._widgetRight.scene.add_geometry("__fetal_head__", self._fetal_head, self._mat)
+        self._widgetRight.scene.add_geometry("__coord_frame__", self._coord_frame, self._mat)
+
+        self._window.post_redraw()
+
+    def update_pose(self):
+        transform = np.eye(4)
+        # transform[:3, :3] = self._fetal_head.get_rotation_matrix_from_xyz((np.pi/180, 0, 0))
+        self._fetal_head.transform(transform)
+
+    def update_colour(self):
+        # vertices = np.asarray(fetal_head.vertices)
+        # # code to paint the head instead of making spheres
+        # vertex_points_to_colour = []
+        # for pin_location in pin_locations:
+        #     point_coord = vertices[pin_location]
+        #     vertex_points_to_colour.append(pin_location)
+        #     for index in range(len(vertices) - 1):
+        #         if np.sqrt((vertices[index][0] - point_coord[0][0]) ** 2 +
+        #                    (vertices[index][1] - point_coord[0][1]) ** 2 +
+        #                    (vertices[index][2] - point_coord[0][2]) ** 2) <= 6:
+        #             vertex_points_to_colour.append(index)
+        #
+        # vertex_colors = np.asarray(fetal_head.vertex_colors)
+        # for vertex in vertex_points_to_colour:
+        #     vertex_colors[vertex] = [1, 0, 0]
+
+        vertex_colors = np.asarray(self._fetal_head.vertex_colors)
+        for vertex in self._head_pin_locations:
+            vertex_colors[vertex] = [1, 0, 0]
+
+        self._fetal_head.vertex_colors = o3d.utility.Vector3dVector(vertex_colors)
 
 @dataclass
 class AccelerationData:
@@ -22,21 +143,25 @@ class AccelerationData:
     y_cal: int
     z_cal: int
 
+
+@dataclass
+class PressureSensorData:
+    back_sensors: np.zeros((13,), dtype=int)
+    right_sensors: np.zeros((13,), dtype=int)
+    left_sensors: np.zeros((13,), dtype=int)
+    front_sensors: np.zeros((13,), dtype=int)
+
+
+@dataclass
+class SerialData:
+    pressure_data: PressureSensorData
+    acceleration_data: AccelerationData
+
+
 def open_serial(ser):
     ser.baudrate = 115200
-    ser.port = ('COM4')
+    ser.port = 'COM4'
     ser.open()
-
-# def calculate_head_rotation(acceleration_data: AccelerationData) -> float:
-#     calibrated_x = (acceleration_data.x - 321) / 67
-#     calibrated_y = (acceleration_data.y - 323.5) / 66.5
-#     return math.atan2(calibrated_x, calibrated_y) * 180 / math.pi - 46
-#
-#
-# def calculate_head_pitch(acceleration_data: AccelerationData) -> float:
-#     calibrated_x = (acceleration_data.x - 321) / 67
-#     calibrated_z = (acceleration_data.z - 290) / 57
-#     return math.atan2(calibrated_x, calibrated_z) * 180 / math.pi - 90
 
 # returns roll, pitch
 def calculate_head_angles(acceleration_data: AccelerationData):
@@ -50,7 +175,21 @@ def calculate_head_angles(acceleration_data: AccelerationData):
                        np.sqrt(acceleration_data.y_cal**2 + acceleration_data.z_cal**2))]
 
 
-def read_from_serial(ser):
+def read_from_serial(ser, data):
+    def calibrate_and_assign_acceleration_data(data_x, data_y, data_z):
+        x = data_x.to_bytes(2, 'big', signed=True)
+        x_raw = (x[0] << 8 | x[1]) >> 4
+        x_cal = (x_raw / (1 << 11))
+
+        y = data_y.to_bytes(2, 'big', signed=True)
+        y_raw = (y[0] << 8 | y[1]) >> 4
+        y_cal = (y_raw / (1 << 11))
+
+        z = data_z.to_bytes(2, 'big', signed=True)
+        z_raw = (z[0] << 8 | z[1]) >> 4
+        z_cal = (z_raw / (1 << 11))
+
+        return AccelerationData(x_raw=x_raw, y_raw=y_raw, z_raw=z_raw, x_cal=x_cal, y_cal=y_cal, z_cal=z_cal)
     def align_to_header(ser):
         header = [0xDE, 0xAD, 0xBE, 0xEF, 0xC0, 0x01, 0xCA, 0xFE]
         max_attempts = 1000
@@ -69,114 +208,38 @@ def read_from_serial(ser):
 
     ser.flushInput()
     align_to_header(ser)
-    NUM_READINGS = 64
-    return unpack(f"<{NUM_READINGS}h", ser.read(NUM_READINGS * 2))
+    num_readings = 64
+    raw_data_array = unpack(f"<{num_readings}h", ser.read(num_readings * 2))
 
+    data.pressure_data.back_sensors = raw_data_array[0:12]
+    data.pressure_data.right_sensors = raw_data_array[16:28]
+    data.pressure_data.left_sensors = raw_data_array[32:44]
+    data.pressure_data.front_sensors = raw_data_array[48:60]
+    data.acceleration_data = calibrate_and_assign_acceleration_data(raw_data_array[13],
+                                                                    raw_data_array[14],
+                                                                    raw_data_array[15])
+    return data
 
-def normalize_data(data):
-    return (data - 0) / (4 - 0)
+def read_head_pin_locations_from_file():
+    pin_locations = []
+    with open(r"pin_locations.txt", "r") as f:
+        for line in f:
+            pin_locations.append([int(e) for e in line.split()])
+    return pin_locations
 
-
-def calibrate_and_assign_acceleration_data(data_x, data_y, data_z):
-    x = data_x.to_bytes(2, 'big', signed=True)
-    x_raw = (x[0] << 8 | x[1]) >> 4
-    x_cal = (x_raw / (1 << 11))
-
-    y = data_y.to_bytes(2, 'big', signed=True)
-    y_raw = (y[0] << 8 | y[1]) >> 4
-    y_cal = (y_raw / (1 << 11))
-
-    z = data_z.to_bytes(2, 'big', signed=True)
-    z_raw = (z[0] << 8 | z[1]) >> 4
-    z_cal = (z_raw / (1 << 11))
-
-    return AccelerationData(x_raw=x_raw, y_raw=y_raw, z_raw=z_raw, x_cal=x_cal, y_cal=y_cal, z_cal=z_cal)
-
-
-if __name__ == "__main__":
+def main():
+    app_instance = AppWindow()
     # Initialise serial
     # ser = serial.Serial()
     # open_serial(ser)
+    # data = SerialData()
+    while app_instance._app.run_one_tick():
+        # read_from_serial(ser, data)
+        app_instance.update_geometry()
 
-    gui.Application.instance.initialize()
-    app = vis_gui.AppWindow(1024, 768)
-    # window = gui.Application.instance.create_window("Birth Simulator", width=1000, height=1000)
-    # widget = gui.SceneWidget()
-    # widget.scene = rendering.Open3DScene(window.renderer)
-    # window.add_child(widget)
-    path = "whole_head_model.STL"
+if __name__ == "__main__":
+    main()
 
-    app.load(path)
-    # Read in file
-    # fetal_head = o3d.io.read_triangle_mesh(path)
-    #
-    # # Initial offset from original model to line up the head with axes of render
-    # # offsets: flexion -0.4 rotation -0.615 deflection -0.2
-    # initial_position = fetal_head.get_rotation_matrix_from_xyz((-0.4, -0.615, -0.2))
-    # fetal_head.rotate(initial_position, fetal_head.get_center())
-    #
-    # # Copy head for dynamic rotation
-    # fetal_head_r = copy.deepcopy(fetal_head)
-    # fetal_head_r.compute_vertex_normals()
-    # fetal_head_r.paint_uniform_color([0.7, 0.7, 0.7])
-    #
-    # material = rendering.MaterialRecord()
-    # material.shader = 'defaultLit'
-    #
-    # # widget.scene.camera.look_at(fetal_head.get_center().tolist(), [1,1,1], [0,0,0])
-    #
-    #
-    # # vis = o3d.visualization.Visualizer()
-    # # vis.create_window(window_name="STL", left=1000, top=200, width=800, height=800)
-    #
-    # coord_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=100, origin=fetal_head_r.get_center())
-    # # vis.add_geometry(coord_frame)
-    # # widget.scene.add_geometry(coord_frame)
-    # widget.scene.add_geometry("head", fetal_head_r, material)
-    # # vis.add_geometry(fetal_head_r)
-
-
-    gui.Application.instance.run()
-
-    # vis.run()
-    # vis.destroy_window()
-    # vertices_list = vis.get_picked_points()
-    #
-    # with open(r"pin_locations.txt", "w") as fp:
-    #     for item in vertices_list:
-    #         fp.write("%s\n" % item.index)
-
-    # points = []
-    # with open(r"pin_locations.txt", "r") as f:
-    #     for line in f:
-    #         points.append([int(e) for e in line.split()])
-    #
-    # vertices = np.asarray(fetal_head_r.vertices)
-    # spheres = []
-    # for point in points:
-    #     point_coord = vertices[point]
-    #     print(point_coord[0][0], point_coord[0][1], point_coord[0][2])
-    #     sphere = o3d.geometry.TriangleMesh.create_sphere(radius=6, resolution=5)
-    #     sphere.translate(np.transpose(point_coord))
-    #     spheres.append(sphere)
-    #     vis.add_geometry(sphere)
-    #
-    # # code to paint the head instead of making spheres
-    # # vertex_points_to_colour = []
-    # # for point in points:
-    # #     point_coord = vertices[point]
-    # #     vertex_points_to_colour.append(point)
-    # #     for index in range(len(vertices) - 1):
-    # #         if np.sqrt((vertices[index][0] - point_coord[0][0])**2 +
-    # #                    (vertices[index][1] - point_coord[0][1])**2 +
-    # #                    (vertices[index][2] - point_coord[0][2])**2) <= 6:
-    # #             vertex_points_to_colour.append(index)
-    # # vertex_colors = np.asarray(fetal_head_r.vertex_colors)
-    #
-    # view_control = vis.get_view_control()
-    # parameters = o3d.io.read_pinhole_camera_parameters("ScreenCamera_2024-02-22-08-39-51.json")
-    # view_control.convert_from_pinhole_camera_parameters(parameters, True)
-    #
     # previous_position = np.identity(3)
     #
     # roll_rolling = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -238,3 +301,46 @@ if __name__ == "__main__":
     #         vis.update_geometry(sphere)
     #     vis.update_renderer()
     #     vis.poll_events()
+
+# another option (slower), that adds spheres where the points are
+# def add_spheres_to_head():
+    # vertices = np.asarray(fetal_head_r.vertices)
+    # spheres = []
+    # for point in points:
+    #     point_coord = vertices[point]
+    #     print(point_coord[0][0], point_coord[0][1], point_coord[0][2])
+    #     sphere = o3d.geometry.TriangleMesh.create_sphere(radius=6, resolution=5)
+    #     sphere.translate(np.transpose(point_coord))
+    #     spheres.append(sphere)
+    #     vis.add_geometry(sphere)
+
+# don't need once already picked points
+# def get_picked_points_from_visualiser(vis):
+#     vertices_list = vis.get_picked_points()
+#
+#     with open(r"pin_locations.txt", "w") as fp:
+#         for item in vertices_list:
+#             fp.write("%s\n" % item.index)
+
+# def calculate_head_rotation(acceleration_data: AccelerationData) -> float:
+#     calibrated_x = (acceleration_data.x - 321) / 67
+#     calibrated_y = (acceleration_data.y - 323.5) / 66.5
+#     return math.atan2(calibrated_x, calibrated_y) * 180 / math.pi - 46
+#
+#
+# def calculate_head_pitch(acceleration_data: AccelerationData) -> float:
+#     calibrated_x = (acceleration_data.x - 321) / 67
+#     calibrated_z = (acceleration_data.z - 290) / 57
+#     return math.atan2(calibrated_x, calibrated_z) * 180 / math.pi - 90
+
+# This works but have to resort to removing/readding geometry because there is no method to update the colors
+# of the mesh in real time for the gui :(
+# However it does work when using the visualiser library
+# def update_geometry(self, fetal_head, i):
+#     transform = np.eye(4)
+#     transform[:3, :3] = fetal_head.get_rotation_matrix_from_xyz((i * np.pi/180, 0, 0))
+#
+#     self._widgetLeft.scene.set_geometry_transform("__fetal_head__", transform)
+#     self._widgetRight.scene.set_geometry_transform("__fetal_head__", transform)
+#
+#     self._window.post_redraw()